68dfed3acf77a57bf657381ebcb342ab8ed5adcc
Rohit Singh <rohit11july2057@gmail.com>
2025-03-04 20:42:56 +0530

    ID's linked

---
+++ baseconfig.py
@@ -0,0 +1,3 @@
+discord_owner_id="Enter the bot's owners ID"
+discord_sdk="Enter your Bot's ID"
+gemini_sdk='Enter your API key'
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import discord
+import baseconfig
+from discord.ext import commands
 
 class Client(discord.Client):
     async def on_ready(self):
@@ -6,6 +8,11 @@ class Client(discord.Client):
 
 intents=discord.Intents.default()
 intents.message_content=True
+bot=commands.Bot(command_prefix='!',intents=intents,help_command=None)
+
+@bot.event
+async def on_ready():
+    print('The bot is now online.')
 
 client=Client(intents=intents)
-client.run('')
+client.run(baseconfig.discord_sdk)
